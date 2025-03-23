8ccde2bd60e60964a5ae5b87a4bff2d4a4f217da
Slavap <slavap@dot.com>
2025-03-23 15:39:38 +0200

    add grpc client project

---
+++ GrpcDemo.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcDemo.Protos;
+
+var clientOptions = new GrpcChannelOptions();
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7222", clientOptions);
+
+var client = new FirstServiceDefinition.FirstServiceDefinitionClient(channel);
