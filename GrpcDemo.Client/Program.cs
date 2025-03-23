31e32a505d64976c31941a3a9821179d84959016
Slavap <slavap@dot.com>
2025-03-23 19:31:38 +0200

    add test implementations of grpc method types

--- GrpcDemo.Client/Program.cs
+++ GrpcDemo.Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcDemo.Protos;
 
@@ -8,3 +9,57 @@ var clientOptions = new GrpcChannelOptions();
 using var channel = GrpcChannel.ForAddress("https://localhost:7222", clientOptions);
 
 var client = new FirstServiceDefinition.FirstServiceDefinitionClient(channel);
+
+UnaryTest(client);
+await ClientStreamingTest(client);
+await ServerStreamingTest(client);
+await DuplexStreamingTest(client);
+
+Console.ReadLine();
+
+void UnaryTest(FirstServiceDefinition.FirstServiceDefinitionClient client)
+{
+    var request = new Request() { Content = "Client" };
+    
+    var response = client.Unary(request);
+    Console.WriteLine(response.Message);
+}
+
+async Task ClientStreamingTest(FirstServiceDefinition.FirstServiceDefinitionClient client)
+{
+    var clientStream = client.ClientStreaming();
+    for (var i = 0; i < 10; i++)
+    {
+        await clientStream.RequestStream.WriteAsync(new Request() { Content = i.ToString() });
+    }
+    await clientStream.RequestStream.CompleteAsync();
+    var response = await clientStream;
+    Console.WriteLine(response.Message);
+}
+
+async Task ServerStreamingTest(FirstServiceDefinition.FirstServiceDefinitionClient client)
+{
+    var request = new Request() { Content = "Client" };
+    using var response = client.ServerStreaming(request);
+
+    await foreach (var responseItem in response.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(responseItem.Message);
+    }
+}
+
+async Task DuplexStreamingTest(FirstServiceDefinition.FirstServiceDefinitionClient client)
+{
+    using var call = client.DuplexStreaming();
+    for (var i = 0; i < 10; i++)
+    {
+        await call.RequestStream.WriteAsync(new Request() { Content = i.ToString() });
+    }
+
+    await foreach (var responseItem in call.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(responseItem.Message);
+    }
+    
+    await call.RequestStream.CompleteAsync();
+}
